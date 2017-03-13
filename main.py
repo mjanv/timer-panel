793e06794bfed173ec4569c67bfd7f71fb2d1bf4
Maxime Janvier <maxime.janvier@gmail.com>
2017-03-14 00:58:29 +0100

    Working version

--- main.py
+++ main.py
@@ -10,25 +10,23 @@ import string
 
 app = Flask(__name__)
 
-
 def get_start(hours, minuts, seconds):
     return datetime.now().replace(hour = int(hours), minute = int(minuts), second = int(seconds))  
 
 @app.route("/")
-def hello():
-    session['times'] = [get_start(23, 5, 0), get_start(23, 0, 0), get_start(23, 5, 24)]
-    return render_template('main.html', starts = session['times'])    
+def hello(): 
+    return render_template('main.html', starts = session.get('times'))    
 
 @app.route("/times", methods=['GET', 'POST'])
 def times():
-    print(request.method)
-    session['times'] = []
+    if not session.get('times'):
+        session['times'] = []
     if request.method == 'POST':
-        d = get_start()
-        d = datetime.now().replace( hour = request.values['hours'],
-                                    minute  = request.values['minuts'],
-                                second  = request.values['seconds'])
-        session['times'] = [d, d]  
+        if request.form['click'] == 'Submit':
+            d = get_start(request.values['hours'], request.values['minuts'], request.values['seconds'])
+            session['times'] = session['times'] + [d]
+        else:
+            session['times'] = [] 
     return render_template('times.html', times = session['times'])     
 
 if __name__ == "__main__":
