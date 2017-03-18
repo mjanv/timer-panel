84263f67edc3f445d6f7b1eaae68e4b232dd0d50
Maxime Janvier <maxime.janvier@gmail.com>
2017-03-18 22:46:30 +0100

    Add vagues names

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from flask import session
 
 from datetime import datetime
 
+import jinja2
+
+
 import random
 import string
 
@@ -18,14 +21,17 @@ def get_start(hours, minuts, seconds):
 
 @app.route("/")
 def hello(): 
-    return render_template('main.html', starts = session.get('times'))    
+    return render_template('main.html', starts = session.get('times'), names = session.get('names'))    
 
 @app.route("/times", methods=['GET', 'POST'])
 def times():
     if request.method == 'POST':
         session['times'] = [] if request.form['click'] == 'Delete All' else session.get('times', []) + get_start(request.values['hours'], request.values['minuts'], request.values['seconds'])
-    return render_template('times.html', times = session.get('times'))     
+        session['names'] = [] if request.form['click'] == 'Delete All' else session.get('names', []) + [request.values['name']]
+    return render_template('times.html', times = session.get('times'), names = session.get('names'))     
 
 if __name__ == "__main__":
+    env = jinja2.Environment()
+    env.globals.update(zip=zip)
     app.secret_key = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10))
     app.run(debug=True)
